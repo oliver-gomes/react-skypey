9681bb377324876e703261aa1a5245194d687747
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-27 00:38:35 -0500

    component created

---
+++ src/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  return <main className="Main">Main Stuff </main>;
+};
+
+export default Main;
---
+++ src/Sidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return <aside className="Sidebar">Sidebar</aside>;
+};
+
+export default Sidebar;
