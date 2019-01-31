df6ce443cf7825f74de3c5dfeaf4d0686b100dd1
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-31 00:13:46 -0500

    User implement

---
+++ src/User.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./User.css";
+
+const User = ({ user }) => {
+  const { name, profile_pic, status } = user;
+
+  return (
+    <div className="User">
+      <img src={profile_pic} alt={name} className="User_pic" />
+      <div className="User__details">
+        <p className="User__details-name">{name}</p>
+        <p className="User__details-status">{status}</p>
+      </div>
+    </div>
+  );
+};
+
+export default User;
