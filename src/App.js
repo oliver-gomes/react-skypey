04092569164be5f0fa91dbb291dd1b25d7f7f991
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-30 23:59:03 -0500

    getState and props to Sidebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import Sidebar from "./Sidebar.js";
 import Main from "./Main.js";
 import "./App.css";
+import store from "./store";
+import _ from "lodash";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Sidebar />
-        <Main />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const { contacts } = store.getState();
+
+  return (
+    <div className="App">
+      <Sidebar contacts={_.values(contacts)} />
+      <Main />
+    </div>
+  );
+};
 
 export default App;
