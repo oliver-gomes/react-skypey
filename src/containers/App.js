057a674028a6e2e97e7dbed9b1289b58cbfdc223
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-31 00:38:52 -0500

    directory update for better organizing files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Sidebar from "./Sidebar.js";
-import Main from "./Main.js";
+import Sidebar from "../components/Sidebar.js";
+import Main from "../components/Main.js";
 import "./App.css";
-import store from "./store";
+import store from "../store";
 import _ from "lodash";
 
 const App = () => {
