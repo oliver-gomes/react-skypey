6c3436ae51570f0ec096331ab2379c689ed987d0
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-30 23:49:50 -0500

    store update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import store from "./store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function fancyLog() {
+  console.log("%c Rendered with 👉 👉👇", "background: purple; color: #FFF");
+  console.log(store.getState());
+}
+
+const render = () => {
+  fancyLog();
+  return ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+render();
+
+store.subscribe(render);
