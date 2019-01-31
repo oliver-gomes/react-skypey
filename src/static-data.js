fd769974296e4da5d85d798ae9e3b04dac0081be
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-30 23:49:56 -0500

    data fix

--- src/static-data.js
+++ src/static-data.js
@@ -1,4 +1,4 @@
-onst shortid = require("shortid"); // shortid.generate() returns a unique "short" id
+const shortid = require("shortid"); // shortid.generate() returns a unique "short" id
 
 const txtgen = require("txtgen"); // txtgen.sentence() returns random "readable" sentences
 
@@ -6,36 +6,25 @@ const faker = require("faker"); // faker is used for generating random fake data
 
 const _ = require("lodash"); // lodash is a utility lib for Javascript
 
-
-
 const users = generateUsers(10);
 
 export const contacts = _.mapKeys(users, "user_id");
 
 export const getMessages = messagesPerUser => {
-
   let messages = {};
 
   _.forEach(users, user => {
-
     messages[user.user_id] = {
-
       ..._.mapKeys(generateMsgs(messagesPerUser), "number")
-
     };
-
   });
 
   return messages;
-
 };
-
-
 
 // just an example of how the state object is structured
 
 export const state = {
-
   user: generateUser(),
 
   messages: getMessages(10),
@@ -45,10 +34,7 @@ export const state = {
   contacts,
 
   activeUserId: null
-
 };
-
-
 
 /**
 
@@ -57,9 +43,7 @@ export const state = {
  */
 
 export function generateUser() {
-
   return {
-
     name: faker.name.findName(),
 
     email: faker.internet.email(),
@@ -69,9 +53,7 @@ export function generateUser() {
     status: txtgen.sentence(),
 
     user_id: shortid.generate()
-
   };
-
 }
 
 /**
@@ -81,17 +63,13 @@ export function generateUser() {
  */
 
 function generateMsg(number) {
-
   return {
-
     number,
 
     text: txtgen.sentence(),
 
     is_user_msg: faker.random.boolean()
-
   };
-
 }
 
 /**
@@ -107,15 +85,9 @@ function generateMsg(number) {
  */
 
 function generateUsers(numberOfUsers) {
-
   return Array.from({ length: numberOfUsers }, () => generateUser());
-
 }
 
-
-
 function generateMsgs(numberOfMsgs) {
-
   return Array.from({ length: numberOfMsgs }, (v, i) => generateMsg(i));
-
 }
