c86586a80d9986199ac669c6de9b57a61fc0326f
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-30 22:42:24 -0500

    createStore initialize

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createStore } from "redux";
+import reducer from "../reducers";
+import { contacts } from "../static-data";
+
+const store = createStore(reducer, { contacts });
+
+export default store;
