3d4c8c2033eb1472d3be1b26f5aaa15f761d98d2
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-01 11:56:58 -0500

    github link added

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ const Header = ({ user }) => {
   const { name, status } = user;
   return (
     <header className="Header">
-      {/* <a href="https://github.com/oliver-gomes/react-skypey" className="github">
+      <a href="https://github.com/oliver-gomes/react-skypey" className="github">
         <i className="fab fa-github" />
-      </a> */}
+      </a>
       <h1 className="Header__name">{name}</h1>
       <p className="Header__status">{status}</p>
     </header>
