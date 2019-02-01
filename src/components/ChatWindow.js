1841c68707785276e8c18fbfd7b6f7a8d17b4405
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-01 00:21:57 -0500

    implement typing

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,14 +1,22 @@
 import React from "react";
 import store from "../store";
-import Header from "../components/Header";
+import Header from "./Header";
+import Chats from "./Chats";
+import MessageInput from "../containers/MessageInput";
+import _ from "lodash";
+import "./ChatWindow.css";
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts.contacts[activeUserId];
+  const activeMsgs = state.messages[activeUserId];
+  const { typing } = state;
 
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
+      <Chats messages={_.values(activeMsgs)} />
+      <MessageInput value={typing} />
     </div>
   );
 };
