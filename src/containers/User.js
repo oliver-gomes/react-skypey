c1226c1dbe16f1a34012fd65d7314d450310fbb0
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-31 20:51:47 -0500

    user update

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./User.css";
+import store from "../store";
+import { setActiveUserId } from "../actions";
+
+const handleUserClick = ({ user_id }) => {
+  store.dispatch(setActiveUserId(user_id));
+};
 
 const User = ({ user }) => {
   const { name, profile_pic, status } = user;
 
   return (
-    <div className="User">
+    <div className="User" onClick={handleUserClick.bind(null, user)}>
       <img src={profile_pic} alt={name} className="User_pic" />
       <div className="User__details">
         <p className="User__details-name">{name}</p>
