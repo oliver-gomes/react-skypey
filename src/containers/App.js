e17378990b48ffa3da7e56cd93c2ad7cbdf1262d
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-31 19:24:29 -0500

    update new changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,12 +6,12 @@ import store from "../store";
 import _ from "lodash";
 
 const App = () => {
-  const { contacts } = store.getState();
+  const { contacts, user, activeUserId } = store.getState();
 
   return (
     <div className="App">
       <Sidebar contacts={_.values(contacts)} />
-      <Main />
+      <Main user={user} activeUserId={activeUserId} />
     </div>
   );
 };
