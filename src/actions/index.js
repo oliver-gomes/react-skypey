9b854dff2f020820aafc0eb4c212e1612878cd80
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-01 00:21:14 -0500

    new action add

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,23 @@
-import { SET_ACTIVE_USER_ID } from "../constants/action-types";
+import {
+  SET_ACTIVE_USER_ID,
+  SET_TYPING_VALUE,
+  SEND_MESSAGE
+} from "../constants/action-types";
 
 export const setActiveUserId = id => ({
   type: SET_ACTIVE_USER_ID,
   payload: id
+});
+
+export const setTypingValue = value => ({
+  type: SET_TYPING_VALUE,
+  payload: value
+});
+
+export const sendMessage = (message, userId) => ({
+  type: SEND_MESSAGE,
+  payload: {
+    message,
+    userId
+  }
 });
