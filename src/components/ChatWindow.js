c8f9a1b1c3c54d6069ad0b575ab758a7cfe2b65f
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-31 20:33:23 -0500

    chatwindow update

---
+++ src/components/ChatWindow.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ChatWindow = ({ activeUserId }) => {
+  return (
+    <div className="ChatWindow">Conversation for user id: {activeUserId}</div>
+  );
+};
+
+export default ChatWindow;
